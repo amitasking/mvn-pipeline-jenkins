9cd156a712ed86c03e89a19be1eca43de12d82a8
amitasking <cu.17bcs1685@gmail.com>
2021-08-13 11:24:26 +0530

    first commit

---
+++ src/main/java/com/vwit/jenkinsmavendemo/JenkinsMavenDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vwit.jenkinsmavendemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsMavenDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsMavenDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/vwit/jenkinsmavendemo/controller/Hello.java
@@ -0,0 +1,15 @@
+package com.vwit.jenkinsmavendemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
+public class Hello {
+	@GetMapping()
+	public String hello() {
+		return "String";
+	}
+}
